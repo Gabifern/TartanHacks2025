520f5b0a78151339f3bbaf714a3a8269779e0b85
GraceSweeney <122422216+GraceSweeney@users.noreply.github.com>
2025-02-07 21:07:45 -0500

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,1 +1,16 @@
+import cv2
 
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()  # Capture frame
+    if not ret:
+        break
+
+    cv2.imshow("Mac Camera Feed", frame)
+
+# Press 'q' to exit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()  # Release the camera
+cv2.destroyAllWindows()  # Close OpenCV windows
